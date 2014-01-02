649219b31b4cbc1801f8ed5fc0e10b191dbc6053
xxyy <xxyy98@gmail.com>
2014-01-03 00:31:36 +0100

    Add code, modify README and gitignore

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-commandblockerultimate
+Command Blocker Ultimate!
 ======================
 
-Ultimate Command Blocker for Bukkti/Spigot.
+A Bukkit/Spigot plugin that is able to block certain (configurable!) commands.
+
+Features
+========
+ - Blocks commands!
+ - Fully configurable!
+ - Open-Source!
+ - Messages can be freely modified!
+ - Lightweight!
+ - Runs on nearly every OS! (Java, you know)
+
+Compilation
+===========
+You could compile it yourself by doing
+````
+git clone https://github.com/xxyy/commandblockerultimate.git
+mvn clean package
+````
+
+Or you could just head over to my Continous Integration Server and get your free executable TODAY!
+
+(But there's more! If you download RIGHT NOW, you can get plenty of other open-source plugins and other stuff FOR FREE at https://github.com/xxyy !!!1)
+
+Support
+=======
+If you have problems, just [join #lit on irc.spi.gt](http://irc.spi.gt/iris/?channels=lit) and ask whatever question you may encounter!
+
+Contribution
+============
+Pull Requests and Issue Reports are greatly appreciated, as long as they follow at least some level of command sense and good practice.
+
+License
+=======
+````
+Command Blocker Ultimate
+Copyright (C) 2014 Philipp Nowak / xxyy (xxyy.github.io)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+````
---
+++ src/main/java/io/github/xxyy/cmdblocker/CommandBlockerPlugin.java
@@ -0,0 +1,95 @@
+/*
+Command Blocker Ultimate
+Copyright (C) 2014 Philipp Nowak / xxyy (xxyy.github.io)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package io.github.xxyy.cmdblocker;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatTabCompleteEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.regex.Pattern;
+
+/**
+ * CommandBlocker Ultimate.
+ * Blocks certain commands.
+ *
+ * @author <a href="http://xxyy.github.io/">xxyy</a>
+ * @since 02.01.14
+ */
+public class CommandBlockerPlugin extends JavaPlugin implements Listener {
+    private static Pattern COMMAND_PATTERN = Pattern.compile("^/((\\S)+)");
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    private boolean canExecute(final CommandSender sender, final String command) {
+        if (!getConfig().getStringList("target-commands").contains(command)) {
+            return true;
+        }
+
+        if (sender instanceof ConsoleCommandSender) {
+            return getConfig().getBoolean("console-bypass", true);
+        } else {
+            return sender.hasPermission(getConfig().getString("bypass-permission"));
+        }
+    }
+
+    private void sendErrorMessageIfEnabled(final CommandSender target) {
+        if (getConfig().getBoolean("show-error-message", true)) {
+            target.sendMessage(
+                    StringEscapeUtils.unescapeHtml(
+                            ChatColor.translateAlternateColorCodes('&', getConfig().getString("error-message"))
+                    )
+            );
+        }
+    }
+
+    private String getRawCommand(final String chatMessage) {
+        return COMMAND_PATTERN.matcher(chatMessage).group(1);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onCommand(final PlayerCommandPreprocessEvent evt) {
+        if (canExecute(evt.getPlayer(), getRawCommand(evt.getMessage()))) {
+            evt.setCancelled(true);
+
+            sendErrorMessageIfEnabled(evt.getPlayer());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onTabComplete(final PlayerChatTabCompleteEvent evt) {
+        if (canExecute(evt.getPlayer(), getRawCommand(evt.getChatMessage()))) {
+            evt.getTabCompletions().clear();
+
+            sendErrorMessageIfEnabled(evt.getPlayer());
+        }
+    }
+}
