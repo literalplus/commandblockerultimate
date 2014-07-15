96bc7e88bd0ab56cc8ba83bf040d721ede5df427
xxyy <devnull@nowak-at.net>
2014-07-15 23:34:56 +0200

    Add plugin class and add bungee dep (related to #7)

---
+++ bungee/src/main/java/io/github/xxyy/cmdblocker/bungee/CommandBlockerPlugin.java
@@ -0,0 +1,12 @@
+package io.github.xxyy.cmdblocker.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * Bungee plugin class for CommandBlockerUltimate.
+ *
+ * @author <a href="http://xxyy.github.io/">xxyy</a>
+ * @since 15.7.14
+ */
+public class CommandBlockerPlugin extends Plugin {
+}
