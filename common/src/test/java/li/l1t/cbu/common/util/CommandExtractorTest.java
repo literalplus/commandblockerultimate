81e7fa80ba250a15d06ba88e7537bfa095094f60
Literallie <git@l1t.li>
2017-08-20 00:16:59 +0200

    Remove duplicated license headers

--- common/src/test/java/li/l1t/cbu/common/util/CommandExtractorTest.java
+++ common/src/test/java/li/l1t/cbu/common/util/CommandExtractorTest.java
@@ -17,25 +17,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-/*
- * Command Blocker Ultimate
- * Copyright (C) 2014-2015 Philipp Nowak / Literallie (xxyy.github.io)
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
 package li.l1t.cbu.common.util;
 
 import org.junit.jupiter.api.Assertions;
